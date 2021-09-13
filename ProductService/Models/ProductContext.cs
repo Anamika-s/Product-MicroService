733dc0492056625af8a0bd18efb586e43b2caf33
anamika.sawhney22@gmail.com <anamika.sawhney22@gmail.com>
2021-09-13 11:01:41 +0000

    Product Service

---
+++ ProductService/Models/ProductContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<Category>()
+                .HasData(
+                new Category() { Id = 1, Name = "Electronics", Description = "Electronics Items" },
+                  new Category() { Id = 2, Name = "Clothes", Description = "Dresses" },
+
+                    new Category() { Id = 3, Name = "Grocery", Description = "Grocery Items" }
+
+                );
+            builder.Entity<Product>()
+                 .HasOne<Category>(s => s.Category)
+                  .WithMany(g => g.Products)
+                  .HasForeignKey(s => s.CategoryId);
+
+
+        }
+    }
+}
---
+++ ProductService/Repository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Repository
+{
+    interface IProductRepository
+    {
+        void DeleteProduct(int productId);
+        Product GetProductByID(int productId);
+        IEnumerable<Product> GetProducts();
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+    }
+}
